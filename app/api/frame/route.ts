eebc494b2b7653591a1e76b9ab95daa0d417cadf
jookido <jookidoso@gmail.com>
2026-01-08 11:41:43 +0700

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,1 +1,31 @@
+import { NextResponse } from "next/server";
 
+const IMAGE_1 =
+  "https://placehold.co/600x400/png?text=Crypto+Pikachu+Game";
+const IMAGE_2 =
+  "https://placehold.co/600x400/png?text=You+Clicked+Play!";
+
+export async function GET() {
+  return NextResponse.json({
+    frames: [
+      {
+        image: IMAGE_1,
+        buttons: [
+          {
+            label: "▶️ Play",
+          },
+        ],
+        post_url: "/api/frame",
+      },
+    ],
+  });
+}
+
+export async function POST(req: Request) {
+  return NextResponse.json({
+    frames: [
+      {
+        image: IMAGE_2,
+        buttons: [
+          {
+            label: "🔁 Restart",
